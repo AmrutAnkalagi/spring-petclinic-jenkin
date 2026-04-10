a1bdcea6016a501e349d483b4308e229362b577e
amrut <R1@gmail.com>
2026-04-10 12:57:12 +0530

    Added groovy files

---
+++ src/vars/simpleFile.groovy
@@ -0,0 +1,31 @@
+def call() {
+    pipeline {
+    agent any
+    tools {
+        maven 'MAVEN_4.0.0'
+    }
+    options {
+        timeout (time:1, unit: 'HOURS')
+    }
+    triggers {
+        pollSCM ('* * * * *')
+    }
+    stages {
+        stage ('SCM') {
+            steps {
+                git url: 'https://github.com/AmrutAnkalagi/spring-petclinic-jenkin.git',
+                    branch: 'main'
+            }   
+        }
+        stage ('Build and Package') {
+            steps {
+                sh 'mvn clean package'
+                
+            }
+                
+        }
+           
+
+    }
+}
+}
